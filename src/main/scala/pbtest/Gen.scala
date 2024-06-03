1b7fe10f6e4ddf8977b469252f09cf66d08b8366
csgn <dev.csgn@gmail.com>
2024-06-03 16:25:28 +0300

    feat: functions

--- src/main/scala/pbtest/Gen.scala
+++ src/main/scala/pbtest/Gen.scala
@@ -4,6 +4,9 @@ import state.{RNG2, State}
 
 import Gen.*
 import Prop.*
+import Prop.Position.{Left, Right}
+import Prop.Result.{Passed, Falsified}
+import scala.util.Failure
 
 object Gen:
   type Gen[+A] = State[RNG2, A]
@@ -14,13 +17,16 @@ object Gen:
   def boolean: Gen[Boolean] =
     State(RNG2.boolean)
 
-  def string(n: Int): Gen[String] =
+  def stringN(n: Int): Gen[String] =
     choose(0, 127)
       .listOfN(n)
-      .map(l => l.map(_.toChar).mkString)
+      .map(_.map(_.toChar).mkString)
 
-  def double(start: Double, stopExclusive: Double): Gen[Double] =
-    State(RNG2.double).map(start + _ % (stopExclusive - start))
+  def double: Gen[Double] =
+    State(RNG2.double)
+
+  def doubleRange(start: Double, stopExclusive: Double): Gen[Double] =
+    double.map(start + _ % (stopExclusive - start))
 
   def option[A](ga: Gen[A]): Gen[Option[A]] =
     boolean.flatMap: b =>
@@ -52,6 +58,33 @@ object Gen:
   def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
     State.sequence(List.fill(n)(g))
 
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
+    Gen.boolean.flatMap: b =>
+      if b then g1
+      else g2
+
+  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] =
+    val g1Threshold = g1(1).abs / (g1(1).abs + g2(1).abs)
+    double.flatMap(w => if w < g1Threshold then g1(0) else g2(0))
+
+  def even(start: Int, stopExclusive: Int): Gen[Int] =
+    choose(
+      start,
+      if stopExclusive % 2 == 0 then stopExclusive - 1
+      else stopExclusive
+    ).map: i =>
+      if i % 2 != 0 then i + 1
+      else i
+
+  def odd(start: Int, stopExclusive: Int): Gen[Int] =
+    choose(
+      start,
+      if stopExclusive % 2 != 0 then stopExclusive - 1
+      else stopExclusive
+    ).map: i =>
+      if i % 2 == 0 then i + 1
+      else i
+
   extension [A](self: Gen[A])
     def flatMap[B](f: A => Gen[B]): Gen[B] =
       State.flatMap(self)(f)
@@ -71,11 +104,81 @@ object Gen:
 end Gen
 
 object Prop:
-  opaque type Prop = Unit
+  opaque type Prop = (TestCases, RNG2) => Result
+
   opaque type SuccessCount = Int
   opaque type FailedCase = String
 
-  def forAll[A](a: Gen[A])(f: A => Boolean): Prop = ???
+  opaque type TestCases = Int
+  object TestCases:
+    extension (x: TestCases) def toInt: Int = x
+    def fromInt(x: Int): TestCases = x
 
-  def check: Either[(FailedCase, SuccessCount), SuccessCount] = ???
+  enum Position:
+    case Left
+    case Right
+
+  enum Result:
+    case Passed
+    case Falsified(
+        failure: FailedCase,
+        successes: SuccessCount,
+        position: Position = Left
+    )
+
+    def isFalsifed: Boolean = this match
+      case Passed             => false
+      case Falsified(_, _, _) => true
+
+  private def randomLazyList[A](g: Gen[A])(rng: RNG2): LazyList[A] =
+    LazyList.unfold(rng)(rng => Some(g.run(rng)))
+
+  private def buildMsg[A](s: A, e: Exception): String =
+    s"""Test Case: $s
+        |Generated an Exception: ${e.getMessage} 
+        |Stack Trace: 
+        |${e.getStackTrace.mkString("\n")}""".stripMargin
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop =
+    (n, rng) =>
+      randomLazyList(as)(rng)
+        .zip(LazyList.from(0))
+        .take(n)
+        .map:
+          case (a, i) =>
+            try
+              if f(a) then Passed
+              else Falsified(a.toString, i)
+            catch
+              case e: Exception =>
+                Falsified(buildMsg(a, e), i)
+        .find(_.isFalsifed)
+        .getOrElse(Passed)
+
+  extension (self: Prop)
+    def &&(that: Prop): Prop =
+      (n, rng) =>
+        self(n, rng) match
+          case Passed =>
+            that(n, rng) match
+              case Passed => Passed
+              case Falsified(f, s, _) =>
+                Falsified(f, s, Right)
+          case Falsified(f, s, _) =>
+            Falsified(f, s, Left)
+
+    def ||(that: Prop): Prop =
+      (n, rng) =>
+        self(n, rng) match
+          case Passed => Passed
+          case Falsified(f, s, _) =>
+            that(n, rng) match
+              case Passed => Passed
+              case Falsified(f, s, _) =>
+                Falsified(f, s, Right)
+
+    def check: Result =
+      val rng = RNG2.Simple2(System.nanoTime)
+      self(1, rng)
+
 end Prop
--- src/test/scala/pbtest/PbtestSuite.scala
+++ src/test/scala/pbtest/PbtestSuite.scala
@@ -6,7 +6,23 @@ import Gen.listOfN
 class PbtestSuite extends munit.FunSuite:
   test("1"):
     val rng = RNG2.Simple2(System.nanoTime())
-    val s =
-      Gen.chooseStartZeroStopInclusive(1).listOfN(Gen.choose(10, 15)).run(rng)
-    println(s._1)
+    // val s =
+    //   Gen.chooseStartZeroStopInclusive(1).listOfN(Gen.choose(10, 15)).run(rng)
+
+    // val s =
+    //   Gen
+    //     .weighted((Gen.choose(0, 5), 0.9), (Gen.choose(5, 10), 0.1))
+    //     .listOfN(10)
+    //     .run(rng)
+
+    // val s = Gen.odd(0, 8).run(rng)
+
+    /* println(Prop.buildMsg(Gen.choose(0, 5), Exception("hello world"))) */
+
+    val g1 = Gen.listOfN(5, Gen.choose(0, 5))
+    val g2 = Gen.listOfN(5, Gen.choose(5, 10))
+    val p = Prop.forAll(g1)(a => 1 == 1)
+      || Prop.forAll(g2)(b => 1 == 1)
+
+    println(p.check)
 end PbtestSuite
