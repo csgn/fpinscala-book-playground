b1d6fa1196c99a53539e446d516b8907af58c7d7
csgn <dev.csgn@gmail.com>
2024-06-10 20:26:34 +0300

    init: Parser

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,14 +4,4 @@ import state.RNG2
 import state.RNG2.Simple2
 
 @main def main(): Unit =
-  val rng = RNG2.Simple2(System.currentTimeMillis)
-  val smallInt = Gen.choose(-10, 10)
-  val maxProp = Prop.forAll(smallInt.nonEmptyList): l =>
-    print("maxProp[F] ")
-    print(l.toString + " ")
-    val max = l.max
-    l.forall(_ <= max)
-
-  print("maxPropRUN ")
-  maxProp.run(MaxSize.fromInt(1), TestCases.fromInt(3), rng = rng)
-  println("DONE")
+  ???
--- src/main/scala/parser/Parser.scala
+++ src/main/scala/parser/Parser.scala
@@ -1,28 +1,64 @@
 package parser
 
-trait Parsers[ParseError, Parser[+_]]:
-  // law: char(c).run(c.toString) == Right(c)
-  // example: char('a').run('a'.toString) == Right('a')
-  def char(c: Char): Parser[Char]
+import pbtest.{Gen, Prop}
+import scala.util.matching.Regex
 
-  // law: string(s).run(s) == Right(s)
+trait Parsers[ParseError, Parser[+_]]:
+  def char(c: Char): Parser[Char] =
+    string(c.toString).map(_.charAt(0))
+
   def string(s: String): Parser[String]
 
-  extension [A](p: Parser[A])
-    // law:
-    //    string(s).or(string(s')).run(s) == Right(s)
-    //    string(s).or(string(s')).run(s') == Right(s')
-    // examples:
-    //    string("abra").or(string("cadabra")).run("abra") == Right("abra")
-    //    string("abra").or(string("cadabra")).run("cadabra") == Right("cadabra")
-    //
-    // law:
-    //    (string(s) | string(s')).run(s) == Right(s)
-    //    (string(s) | string(s')).run(s') == Right(s')
-    // examples:
-    //    (string("abra") | string("cadabra")).run("abra") == Right("abra")
-    //    (string("abra") | string("cadabra")).run("cadabra") == Right("cadabra")
-    infix def or(p2: Parser[A]): Parser[A]
-    def |(p2: Parser[A]): Parser[A] = p.or(p2)
+  def succeed[A](a: A): Parser[A] =
+    string("").map(_ => a)
+
+  def defer[A](p: => Parser[A]): Parser[A]
+
+  def regex(r: Regex): Parser[String]
+
+  def fail(s: String): Parser[String]
+
+  val nonNegativeInt: Parser[Int] =
+    for
+      nString <- regex("[0-9]+".r)
+      n <- nString.toIntOption match
+        case Some(n) => succeed(n)
+        case None    => fail("expected an integer")
+    yield n
+
+  extension [A](p1: Parser[A])
+    infix def or(p2: => Parser[A]): Parser[A]
+    def |(p2: => Parser[A]): Parser[A] = or(p2)
+
+    infix def product[B](p2: => Parser[B]): Parser[(A, B)] =
+      for
+        a <- p1
+        b <- p2
+      yield (a, b)
+
+    def **[B](p2: => Parser[B]): Parser[(A, B)] = product(p2)
+
+    def flatMap[B](f: A => Parser[B]): Parser[B]
+
+    def map[B](f: A => B): Parser[B] =
+      flatMap(a => succeed(f(a)))
+
+    def map2[B, C](p2: => Parser[B])(f: (A, B) => C): Parser[C] =
+      for
+        a <- p1
+        b <- p2
+      yield f(a, b)
+
+    def listOfN(n: Int): Parser[List[A]] =
+      if n <= 0 then succeed(Nil)
+      else map2(listOfN(n - 1))(_ :: _)
+
+    def many: Parser[List[A]] =
+      map2(p1.many)(_ :: _) | succeed(Nil)
+
+    def many1: Parser[List[A]] =
+      map2(p1.many)(_ :: _)
+
+    def slice: Parser[String]
 
     def run(input: String): Either[ParseError, A]
